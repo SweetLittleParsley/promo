43fed49fa7e1137cd26f38fa1a3052397fde75ed
João Amaral <j_amaral@live.com.pt>
2014-09-12 11:40:51 +0100

    Promocode install generator fixes

--- lib/generators/promo/install/install_generator.rb
+++ lib/generators/promo/install/install_generator.rb
@@ -11,9 +11,15 @@ module Promo
 
       def create_migration_files
         time = Time.now.strftime("%Y%m%d%H%M%S").to_i
-        template '1_create_promo_promocode.rb', File.join('db', 'migrate', "#{time}_create_promo_promocode.rb")
-        template '2_create_promo_history.rb', File.join('db', 'migrate', "#{time+1}_create_promo_history.rb")
-        template '3_add_minimum_purchase_value_for_promocode.rb', File.join('db', 'migrate', "#{time+2}_add_minimum_purchase_value_for_promocode.rb")
+        if Dir[File.join('db', 'migrate', '*_create_promo_promocode*')].empty?
+          template '1_create_promo_promocode.rb', File.join('db', 'migrate', "#{time}_create_promo_promocode.rb")
+        end
+        if Dir[File.join('db', 'migrate', '*_create_promo_history*')].empty?
+          template '2_create_promo_history.rb', File.join('db', 'migrate', "#{time+1}_create_promo_history.rb")
+        end
+        if Dir[File.join('db', 'migrate', '*_add_minimum_purchase_value_for_promocode*')].empty?
+          template '3_add_minimum_purchase_value_for_promocode.rb', File.join('db', 'migrate', "#{time+2}_add_minimum_purchase_value_for_promocode.rb")
+        end
       end
 
     end
